88caf7b0454cf997ddf8be01746148ae154d4047
alexwortega <alexdragannikolich@gmail.com>
2019-09-08 16:04:28 +0300

    First
    ff

---
+++ TrustedPLM/Assets/Scripts/RowScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowScript : MonoBehaviour
+{
+     string url = "vk.com";
+    public GameObject rowPrefab;
+    public GameObject rowPrefab2;
+    public float subrowOffset = 25f;
+//    private float parentRowPosX;
+//    private float parentRowPosY;
+//    private float parentRowWidth;
+//    private float parentRowHeight;
+    private RectTransform parentRow;
+    public float spaceBetweenRows = 0.07f;
+    public GameObject parentContainer;
+    public List<GameObject> nextRows = new List<GameObject>();
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentRow = gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ExpandRow()
+    {
+
+        GameObject newRow = Instantiate(rowPrefab, parentContainer.transform);
+        RectTransform newRowPos = newRow.GetComponent<RectTransform>();
+        newRowPos.localPosition = new Vector3(parentRow.localPosition.x + subrowOffset,
+                parentRow.localPosition.y - parentRow.rect.height - spaceBetweenRows); 
+        
+        GameObject newRow2 = Instantiate(rowPrefab2, parentContainer.transform);
+        RectTransform newRowPos2 = newRow2.GetComponent<RectTransform>();
+        newRowPos2.localPosition = new Vector3(parentRow.localPosition.x + subrowOffset,
+            parentRow.localPosition.y - parentRow.rect.height*2 - spaceBetweenRows*2);
+        
+       // Application.OpenURL(url);
+                                      
+      // offset following rows
+        foreach (GameObject row in nextRows)
+        {
+            row.GetComponent<RectTransform>().localPosition = new Vector3( 
+                row.GetComponent<RectTransform>().localPosition.x,
+                row.GetComponent<RectTransform>().localPosition.y 
+                 - 2*parentRow.rect.height - 2*spaceBetweenRows);
+        }
+        
+    }
+}
---
+++ TrustedPLM/Assets/Scripts/Waves.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+//using UnityEngine.JSON
+[Serializable]
+public class PlayerInfo
+{
+    public string water;
+    public int energy;
+    public float health;
+ 
+    public static PlayerInfo CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<PlayerInfo>(jsonString);
+    }
+}
+ 
+public class Waves : MonoBehaviour
+{
+    //UIELEMNTS
+    public GameObject Confiramtion;
+    public string returned;
+    public Canvas comment;
+    public Button send;
+    public Button agreement;
+    public GameObject parent;
+    public InputField input1;
+    public InputField input2;
+    // public string SaveToString()
+
+    //   return JsonUtility.ToJson(this);
+    string sender;
+    public void InstButton() {
+        Instantiate(Confiramtion, parent.transform);
+    }
+
+     public void SendStuff()
+     {
+        Whattosend("auditor");
+        Whattosend("Труба 377х9 с бетонным покрытием в оцинкованной оболочке " + input1.text);
+        
+     }
+
+
+
+    public void SendFactory() {
+        Whattosend("zavod");
+        Whattosend("Truba trexsloenaya s politileonom");
+    } 
+
+     //
+    public string Whattosend( string input) {
+        sender = input;
+       // Debug.Log(sender+"whatotsend");
+        return sender;
+    }
+   
+ 
+    void Start()
+    {
+       // Whattosend("Труба 377х9 с бетонным покрытием в оцинкованной оболочке"+input1.text);
+        StartCoroutine(GetRequest("http://37.204.92.227:8087/" + sender ));
+     
+    }
+    IEnumerator Upload()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("fa"));
+        //formData.Add(new MultipartFormFileSection("volni_sosut", "myfile.txt"));
+ 
+        UnityWebRequest www = UnityWebRequest.Post("http://37.204.92.227:8087/", formData);
+        yield return www.SendWebRequest();
+ 
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+ 
+        }
+    }
+ 
+    IEnumerator GetRequest(string uri)
+    {
+        UnityWebRequest uwr = UnityWebRequest.Get(uri);
+        yield return uwr.SendWebRequest();
+ 
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+            Console.Write("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            returned = uwr.downloadHandler.text;
+ 
+            //uwr.downloadHandler.text
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            Console.Write(uwr.downloadHandler.text);
+        }
+    }
+}
